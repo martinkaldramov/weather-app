3d8bd34a3cfc2ca6d0a78cad8e8c371d7b5b1513
Marto <martinkaldramov1@gmail.com>
2017-09-05 17:08:38 +0300

    added dynamic address by encoding it wtih encodeURIComponent

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch weather for",
+      string: true
+    }  
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+const encodedAddress = decodeURIComponent(argv.a);
+
+console.log(argv);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
   }, (error, response, body) => {
-    console.log(JSON.stringify(error, undefined, 2));
+    console.log(`The Formatted Address is: ${body.results[0].formatted_address}`);
+    console.log(`The LAT is: ${body.results[0].geometry.location.lat}`);
+    console.log(`The LNG is: ${body.results[0].geometry.location.lng}`);
   });
