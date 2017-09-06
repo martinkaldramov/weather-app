08e5b47dfa1fdccfbed439583d708a17c44f1ebb
Marto <martinkaldramov1@gmail.com>
2017-09-06 20:35:41 +0300

    testing the Dark Sky API

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
+// const request = require('request');
+// const yargs = require('yargs');
+// 
+// const geocode = require('./geocode/geocode');
+// 
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: "address",
+//       describe: "Address to fetch weather for",
+//       string: true
+//     }  
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+// 
+// geocode.geocodeAddress(argv.a, (errorMessage, results) => {
+//   if(errorMessage){
+//     console.log(errorMessage);  
+//   }else{
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// }); 
+
 const request = require('request');
-const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode');
-
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: "address",
-      describe: "Address to fetch weather for",
-      string: true
-    }  
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
-
-geocode.geocodeAddress(argv.a); 
+request({
+    url: 'https://api.darksky.net/forecast/5ef948d7fff5094cb8f5607aef5afa12/42.6977082,23.3218675',
+    json: true
+  }, 
+  (error, response, body) => {
+    console.log(body);
+});
