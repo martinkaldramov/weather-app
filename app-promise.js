0c16f4e126b2a84b8ace622ecde3da2731d3f186
Marto <martinkaldramov1@gmail.com>
2017-09-09 21:33:58 +0300

    using axios to fetch the data

--- app-promise.js
+++ app-promise.js
@@ -1,4 +1,5 @@
 const yargs = require('yargs');
+const axios = require('axios');
 
 const argv = yargs
   .options({
@@ -13,3 +14,18 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
+var encodedAddress = encodedAddress = decodeURIComponent(argv.address);
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+axios.get(geocodeUrl).then((response) => {
+  if(response.data.status === 'ZERO_RESULTS'){
+   throw new Error('Unable to find the address'); 
+  }
+  console.log(response.data);
+}).catch((e) => {
+ if(e.code === 'ENOTFOUND'){
+  console.log('Unable to connect to API servers');  
+ }else{
+   console.log(e.message);  
+ } 
+});
